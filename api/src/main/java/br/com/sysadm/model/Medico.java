b1b26d4a51b7d4886c8938fea4d7e8a0ef4cad71
Jefferson Sant'ana Galvão <jeffer_sg@hotmail.com>
2024-05-13 21:53:17 -0300

    garantir unicidade em cpf na entidade medico

--- api/src/main/java/br/com/sysadm/model/Medico.java
+++ api/src/main/java/br/com/sysadm/model/Medico.java
@@ -1,5 +1,6 @@
 package br.com.sysadm.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,8 @@ public class Medico {
     private Long id;
 
     private String nome;
+
+    @Column(unique = true, nullable = false)
     private String cpf;
     private String especialidade;
 

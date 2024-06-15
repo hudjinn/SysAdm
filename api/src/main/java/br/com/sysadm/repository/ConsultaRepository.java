3704947b03be3abceca15c7003f91419f9f1fa81
Jefferson Sant'ana Galvão <jeffer_sg@hotmail.com>
2024-06-14 23:09:26 -0300

    correção no repositorio consulta

--- api/src/main/java/br/com/sysadm/repository/ConsultaRepository.java
+++ api/src/main/java/br/com/sysadm/repository/ConsultaRepository.java
@@ -1,12 +1,10 @@
 package br.com.sysadm.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.sysadm.model.Consulta;
-import br.com.sysadm.model.Imc;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
